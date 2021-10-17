eb06ffe3e7f150472a7ede437a42447a72a234e4
Seumeng <seumeng34@gmail.com>
2021-10-17 21:29:35 +0700

    first commit

---
+++ JavaScript/sideBar.js
@@ -0,0 +1,53 @@
+const shrink_btn = document.querySelector(".shrink-btn");
+const search = document.querySelector(".search");
+const sidebar_links = document.querySelectorAll(".sidebar-links a");
+const active_tab = document.querySelector(".active-tab");
+const shortcuts = document.querySelector(".sidebar-links h4");
+const tooltip_elements = document.querySelectorAll(".tooltip-element");
+
+let activeIndex;
+
+shrink_btn.addEventListener("click", () => {
+  document.body.classList.toggle("shrink");
+  setTimeout(moveActiveTab, 400);
+
+  shrink_btn.classList.add("hovered");
+
+  setTimeout(() => {
+    shrink_btn.classList.remove("hovered");
+  }, 500);
+});
+
+function changeContent(fileName) {
+  document.getElementById("child-view").innerHTML = '<object type="text/html" data="../views/'+fileName +'"></object>';
+}
+
+function moveActiveTab() {
+  let topPosition = activeIndex * 58 + 2.5;
+
+  active_tab.style.top = `${topPosition}px`;
+}
+
+function changeLink() {
+  sidebar_links.forEach((sideLink) => sideLink.classList.remove("active"));
+  this.classList.add("active");
+
+  activeIndex = this.dataset.active;
+
+  moveActiveTab();
+}
+
+sidebar_links.forEach((link) => link.addEventListener("click", changeLink));
+
+function showTooltip() {
+  let tooltip = this.parentNode.lastElementChild;
+  let spans = tooltip.children;
+  let tooltipIndex = this.dataset.tooltip;
+
+
+  tooltip.style.top = `${(100 / (spans.length * 2)) * (tooltipIndex * 2 + 1)}%`;
+}
+
+tooltip_elements.forEach((elem) => {
+  elem.addEventListener("mouseover", showTooltip);
+});
